cedcfbeea5ef90213d6280230524fe218d97dee4
het3rias <benjaminuazo2021@gmail.com>
2025-04-22 16:26:28 -0400

    ejercicicos for

---
+++ for.py
@@ -0,0 +1,68 @@
+# nombre=input("ingrese su nombre")
+# edad=input("ingrese su edad")
+
+
+# print("hola ",nombre,"su edad es ",edad)
+# print(f"hola {nombre} su edad es {edad}") 
+# tablas
+# num=0
+#tablas
+# num=int(input("ingrese un numero "))
+ 
+# for i in range (1, 11) :
+#     print(i,"x",num,"=",i*num)
+
+
+# for i in range (1, 11) :
+#     print("esta es la tabla de ",i)
+#     for e in range (1,11):
+#         print(i,"x",e,"=",i*e)
+
+# expliacion y uso del for 
+# for i in range(3):
+#     print("hola")
+
+
+# for i in range(5):
+#     print(i+1)  
+
+
+# cant=int(input("ingrese un la cantidad de repeticiones "))
+
+# for i in range(cant):
+#     print("repeticion", i+1)
+
+
+# cant=int(input("ingrese la canctidad de notas "))
+# total=0
+# notasazules=0
+# for i in range(cant):
+#     print("ingrese la nota ",i+1)
+#     nota=float(input())
+#     total=total+nota    
+#     if nota>=4:
+#         notasazules+=1
+# prom=total/cant
+
+# print(f"su promedio es {round(prom,1)}")
+# print("notas azules ",notasazules)
+# if prom>=4.0:
+#     print("aprobo")
+# else:
+#     print("reprobo")
+
+
+# clave=1234
+
+
+
+# for i in range(3):
+#     pas=int(input("ingrese la password: "))
+
+#     if pas==clave:
+#         print("bienvenido")
+#         break
+#     else:
+#         print("fuera de aqui ctm")
+
+   
