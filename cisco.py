cdf5ae989b026a2e8d8231eede0019f826289c18
het3rias <benjaminsuazo2021@gmail.com>
2025-04-24 21:46:09 -0400

    python

---
+++ cisco.py
@@ -0,0 +1,40 @@
+# i = 0
+# while i <= 3 :
+#     i += 2
+#     print("*")
+# i = 0
+# while i <= 5 :
+#     i += 1
+#     if i % 2 == 0:
+#       break
+#     print("*")
+# for i in range(1):
+#     print("#")
+# else:
+#     print("#")
+# var = 0
+# while var < 6:
+#     var += 1
+#     if var % 2 == 0:
+#         continue
+#     print("#")
+
+
+# a = 1
+# b = 0
+# c = a & b
+# d = a | b
+# e = a ^ b
+ 
+# print(c + d + e)
+# my_list_1 = [1, 2, 3]
+# my_list_2 = []
+# for v in my_list_1:
+#     my_list_2.insert(0, v)
+# print(my_list_2)
+# my_list = [1, 2, 3]
+# for v in range(len(my_list)):
+#     my_list.insert(1, my_list[v])
+# print(my_list)
+my_list = [[0, 1, 2, 3] for i in range(2)]
+print(my_list[2][0])
--- prueba3.py
+++ prueba3.py
@@ -23,9 +23,52 @@
 
 
 
-num=0
+# num=0
 
-num=int(input("ingrese un numero"))
+# num=int(input("ingrese un numero "))
+ 
+# for i in range (1, 11) :
+#     print(i,"x",num,"=",i*num)
 
-for i in range(10):
-    for e in range
+
+# for i in range (1, 11) :
+#     print("esta es la tabla de ",i)
+#     for e in range (1,11):
+#         print(i,"x",e,"=",i*e)
+# # calculadora basica 
+
+# op=0
+# num=0
+# num2=0
+    
+
+# while op!=5:
+#     print("bienvenido a la calduladora")
+#     print("1. suma\n2. multiplicacion\n3. resta\n4. division\n5.salir")
+#     op=int(input("ingrese opcion "))
+
+#     if op==1:
+#         num=int(input("ingrese numero "))
+#         num2=int(input("ingrese numero "))
+#         suma=num +num2
+#         print("resultado ",suma)
+#     elif op==2:
+#         num=int(input("ingrese numero "))
+#         num2=int(input("ingrese numero "))
+#         multiplicacion=num*num2
+#         print("resultado ",multiplicacion)
+#     elif op==3:
+#         num=int(input("ingrese numero "))
+#         num2=int(input("ingrese numero "))
+#         resta=num-num2
+#         print("resultado ",resta)
+#     elif op==4:
+#         num=int(input("ingrese numero "))
+#         num2=int(input("ingrese numero "))
+#         division=num/num2
+#         print("resultado ",division)
+#     elif op==5:
+#         print("saliendo")
+#     else:
+#         print("ingrese opcion valida")
+       
